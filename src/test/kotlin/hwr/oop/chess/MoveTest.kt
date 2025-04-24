22f24e8a234e0098c4ffdf4bca35b27aa3c94a02
Schiriki <schiriki@raps2.de>
2025-04-24 14:14:38 +0200

    feat/implement-pieces (#8)
    * Start structure
    
    Co-authored-by: Nicole Zimmermann <45459028+NicoleZi@users.noreply.github.com>
    
    * Implement draft for move logic
    
    * Refactor Position to represent right order
    
    * Implement black pawn test & refactor
    
    * Fix move creation for pieces
    
    * Simplify move creation for pawn
    
    * Add Bishop & improve move
    
    ---------
    
    Co-authored-by: Nicole Zimmermann <45459028+NicoleZi@users.noreply.github.com>

---
+++ src/test/kotlin/hwr/oop/chess/MoveTest.kt
@@ -0,0 +1,16 @@
+package hwr.oop.chess
+
+import io.kotest.core.spec.style.AnnotationSpec
+import org.assertj.core.api.Assertions.assertThat
+
+class MoveTest : AnnotationSpec() {
+  @Test
+  fun `Test move creation`() {
+    val start = Position('a', 2)
+    val end = Position('a', 3)
+    val move = Move(start, end)
+
+    assertThat(start).isEqualTo(move.start)
+    assertThat(end).isEqualTo(move.end)
+  }
+}
--- src/test/kotlin/hwr/oop/chess/RookTest.kt
+++ src/test/kotlin/hwr/oop/chess/RookTest.kt
@@ -1,12 +1,39 @@
 package hwr.oop.chess
 
 import io.kotest.core.spec.style.AnnotationSpec
+import org.assertj.core.api.Assertions.assertThat
 
-class RookTest: AnnotationSpec() {
+class RookTest : AnnotationSpec() {
   @Test
-  fun `Test rook creation`(){
-    val rook = Rook(true)
-    assert(rook.isWhite)
-    assert(!rook.isCaptured)
+  fun `Test rook creation`() {
+    val isWhite = true
+    val rook = Rook(isWhite)
+    assertThat(rook.isWhite).isTrue
+    assertThat(rook.isCaptured).isFalse
+  }
+
+  @Test
+  fun `Test rook movement from d4`() {
+    val isWhite = true
+    val rook = Rook(isWhite)
+    val position = Position('d', 4)
+    val moves = rook.move(position)
+
+    assertThat(moves).containsExactlyInAnyOrder(
+      Position('d', 1),
+      Position('d', 2),
+      Position('d', 3),
+      Position('d', 5),
+      Position('d', 6),
+      Position('d', 7),
+      Position('d', 8),
+      Position('a', 4),
+      Position('b', 4),
+      Position('c', 4),
+      Position('e', 4),
+      Position('f', 4),
+      Position('g', 4),
+      Position('h', 4)
+    )
   }
 }
