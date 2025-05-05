2dab950836a93beb34e2d740c865b4c79beec8cc
Florimond <florimond.gienow@spree-media.net>
2025-05-05 12:50:26 +0200

    Changes by IskandarA

--- src/main/kotlin/hwr/oop/group8/chess/piece/Knight.kt
+++ src/main/kotlin/hwr/oop/group8/chess/piece/Knight.kt
@@ -2,11 +2,29 @@ package hwr.oop.group8.chess.piece
 
 import hwr.oop.group8.chess.Board
 import hwr.oop.group8.chess.Color
+import hwr.oop.group8.chess.Direction
 import hwr.oop.group8.chess.Move
 
 class Knight(override val color: Color) : Piece {
   override fun isMoveValid(move: Move, board: Board): Boolean {
-    TODO("Not yet implemented")
+    var from = move.from
+    val to = move.to
+    val direction = move.getMoveDirection()
+    from = from.getAdjacentPosition(direction)
+    listOf(
+      listOf(Direction.TOP, Direction.TOP_RIGHT),
+      listOf(Direction.TOP, Direction.TOP_LEFT),
+      listOf(Direction.BOTTOM, Direction.BOTTOM_RIGHT),
+      listOf(Direction.BOTTOM, Direction.BOTTOM_LEFT),
+      listOf(Direction.RIGHT, Direction.BOTTOM_RIGHT),
+      listOf(Direction.RIGHT, Direction.TOP_RIGHT),
+      listOf(Direction.LEFT, Direction.BOTTOM_LEFT),
+      listOf(Direction.LEFT, Direction.TOP_LEFT)
+    )
+    // Kombination von moves als ein Path bzw eine Strecke speichern
+
+
+    return true
   }
 
   override fun getChar(): Char {
--- src/main/kotlin/hwr/oop/group8/chess/piece/Queen.kt
+++ src/main/kotlin/hwr/oop/group8/chess/piece/Queen.kt
@@ -6,7 +6,31 @@ import hwr.oop.group8.chess.Move
 
 class Queen(override val color: Color) : Piece {
   override fun isMoveValid(move: Move, board: Board): Boolean {
-    TODO("Not yet implemented")
+    var from = move.from
+    val to = move.to
+    val direction = move.getMoveDirection()
+
+    if (move.isMoveStraight()) {
+      check(move.isMoveStraight()) { "Invalid move for piece Queen from $from to $to" }
+      from = from.getAdjacentPosition(direction) // Skip current square
+      while (from != to) {
+        if (board.getSquare(from).getPiece() != null) {
+          return false
+        }
+        from = from.getAdjacentPosition(direction)
+      }
+    }
+    else {
+      check(move.isMoveDiagonal()) { "Invalid move for piece Queen from $from to $to" }
+      from = from.getAdjacentPosition(direction) // Skip current square
+      while (from != to) {
+        if (board.getSquare(from).getPiece() != null) {
+          return false
+        }
+        from = from.getAdjacentPosition(direction)
+      }
+    }
+    return true
   }
 
   override fun getChar(): Char {
