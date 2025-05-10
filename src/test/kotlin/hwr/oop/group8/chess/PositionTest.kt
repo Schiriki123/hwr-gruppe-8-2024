d76c661f4b57709a69f17a297fd7d1a96025d82c
Schiriki <schiriki@raps2.de>
2025-05-10 17:44:18 +0200

    Remove deprecated getAdjacentPosition method

--- src/test/kotlin/hwr/oop/group8/chess/PositionTest.kt
+++ src/test/kotlin/hwr/oop/group8/chess/PositionTest.kt
@@ -29,27 +29,4 @@ class PositionTest : AnnotationSpec() {
       Position('i', 1)
     }
   }
-
-  @Test
-  fun `Test getting adjacent positions`() {
-    val position = Position('d', 4)
-
-    val topPosition = position.getAdjacentPosition(Direction.TOP)
-    val bottomPosition = position.getAdjacentPosition(Direction.BOTTOM)
-    val leftPosition = position.getAdjacentPosition(Direction.LEFT)
-    val rightPosition = position.getAdjacentPosition(Direction.RIGHT)
-    val topLeftPosition = position.getAdjacentPosition(Direction.TOP_LEFT)
-    val topRightPosition = position.getAdjacentPosition(Direction.TOP_RIGHT)
-    val bottomLeftPosition = position.getAdjacentPosition(Direction.BOTTOM_LEFT)
-    val bottomRightPosition = position.getAdjacentPosition(Direction.BOTTOM_RIGHT)
-
-    assertThat(topPosition).isEqualTo(Position('d', 5))
-    assertThat(bottomPosition).isEqualTo(Position('d', 3))
-    assertThat(leftPosition).isEqualTo(Position('c', 4))
-    assertThat(rightPosition).isEqualTo(Position('e', 4))
-    assertThat(topLeftPosition).isEqualTo(Position('c', 5))
-    assertThat(topRightPosition).isEqualTo(Position('e', 5))
-    assertThat(bottomLeftPosition).isEqualTo(Position('c', 3))
-    assertThat(bottomRightPosition).isEqualTo(Position('e', 3))
-  }
 }
