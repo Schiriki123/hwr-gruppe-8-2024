2b23989d3acb3bf196527213e30d5c38d6404d2e
Schiriki <schiriki@raps2.de>
2025-05-19 10:41:39 +0200

    Complete refactor direction position nextPosition()

--- src/test/kotlin/hwr/oop/group8/chess/core/PositionTest.kt
+++ src/test/kotlin/hwr/oop/group8/chess/core/PositionTest.kt
@@ -29,4 +29,92 @@ class PositionTest : AnnotationSpec() {
       Position('i', 1)
     }
   }
+
+  @Test
+  fun `test next position top`() {
+    val position = Position('e', 4)
+    val direction = Direction.TOP
+
+    val expectedPosition = Position('e', 5)
+    val newPosition = position.nextPosition(direction)
+
+    assertThat(newPosition).isEqualTo(expectedPosition)
+  }
+
+  @Test
+  fun `test next position bottom`() {
+    val position = Position('e', 4)
+    val direction = Direction.BOTTOM
+
+    val expectedPosition = Position('e', 3)
+    val newPosition = position.nextPosition(direction)
+
+    assertThat(newPosition).isEqualTo(expectedPosition)
+  }
+
+  @Test
+  fun `test next position left`() {
+    val position = Position('e', 4)
+    val direction = Direction.LEFT
+
+    val expectedPosition = Position('d', 4)
+    val newPosition = position.nextPosition(direction)
+
+    assertThat(newPosition).isEqualTo(expectedPosition)
+  }
+
+  @Test
+  fun `test next position right`() {
+    val position = Position('e', 4)
+    val direction = Direction.RIGHT
+
+    val expectedPosition = Position('f', 4)
+    val newPosition = position.nextPosition(direction)
+
+    assertThat(newPosition).isEqualTo(expectedPosition)
+  }
+
+  @Test
+  fun `test next position top left`() {
+    val position = Position('e', 4)
+    val direction = Direction.TOP_LEFT
+
+    val expectedPosition = Position('d', 5)
+    val newPosition = position.nextPosition(direction)
+
+    assertThat(newPosition).isEqualTo(expectedPosition)
+  }
+
+  @Test
+  fun `test next position top right`() {
+    val position = Position('e', 4)
+    val direction = Direction.TOP_RIGHT
+
+    val expectedPosition = Position('f', 5)
+    val newPosition = position.nextPosition(direction)
+
+    assertThat(newPosition).isEqualTo(expectedPosition)
+  }
+
+  @Test
+  fun `test next position bottom left`() {
+    val position = Position('e', 4)
+    val direction = Direction.BOTTOM_LEFT
+
+    val expectedPosition = Position('d', 3)
+    val newPosition = position.nextPosition(direction)
+
+    assertThat(newPosition).isEqualTo(expectedPosition)
+  }
+
+  @Test
+  fun `test next position bottom right`() {
+    val position = Position('e', 4)
+    val direction = Direction.BOTTOM_RIGHT
+
+    val expectedPosition = Position('f', 3)
+    val newPosition = position.nextPosition(direction)
+
+    assertThat(newPosition).isEqualTo(expectedPosition)
+  }
 }
